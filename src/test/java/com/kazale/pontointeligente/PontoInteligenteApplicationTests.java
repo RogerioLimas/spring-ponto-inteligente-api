621265191279fdd07287b6213b5a627025003c92
Rogério Limas Moreira <rogerio.moreira@visto.global>
2020-08-22 14:52:03 -0300

    Commit inicial dos arquivos do projeto

---
+++ src/test/java/com/kazale/pontointeligente/PontoInteligenteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kazale.pontointeligente;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PontoInteligenteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
